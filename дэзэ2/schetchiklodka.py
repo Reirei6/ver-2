57b7679a255b4e4b4954d2b23e85f4a0048b3a0b
Reirei6 <143934507+Reirei6@users.noreply.github.com>
2023-12-29 19:21:44 +0300

    Add files via upload

---
+++ дэзэ2/schetchiklodka.py
@@ -0,0 +1,17 @@
+f = open("train.csv", "r")
+lst = list(f.readlines())
+f.close()
+
+all = 0
+male = 0
+female = 0
+
+m = []
+for i in range(1, len (spisok)):
+    m = lst[i].split(',')
+    if m[1] == '1' and m[5] == 'male':
+       male += 1
+    if m[1] == '1' and m[5] == 'female':
+        female += 1
+all = male + female
+print("общее число выживших:", all , "," " выживших мужчин:", male , "," " выживших женщин:", female)
